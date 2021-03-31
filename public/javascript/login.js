147233c150c0e7e33f372fc47c3ef9da7bb394f4
Bretbret18 <bret.sperry@gmail.com>
2021-03-31 17:40:22 -0400

    end four

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,11 +1,11 @@
 async function loginFormHandler(event) {
   event.preventDefault();
 
-  const email = document.querySelector('#email-signup').value.trim();
-  const password = document.querySelector('#password-signup').value.trim();
+  const email = document.querySelector('#email-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
 
   if (email && password) {
-   const response = await fetch('/api/users/login', {
+   const response = await fetch('/api/users/login', { 
       method: 'post',
       body: JSON.stringify({
         email,
@@ -26,7 +26,7 @@ async function loginFormHandler(event) {
 async function signupFormHandler(event) {
     event.preventDefault();
   
-    const username = document.querySelector('#username-signup').value.trim();
+    const username = document.querySelector('#username-signup').value.trim(); // addressing proper tags?
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
   
@@ -50,7 +50,7 @@ async function signupFormHandler(event) {
     }
   }
 
-  document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+  document.querySelector('#login-form').addEventListener('submit', loginFormHandler);
   
-  document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
+  document.querySelector('#signup-form').addEventListener('submit', signupFormHandler);
   
--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
-const exphbs = require('express-handlebars')
+const exphbs = require('express-handlebars');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 
 const sess = {
     secret: 'Super secret secret',
@@ -21,7 +23,9 @@ const sess = {
 
 app.use(session(sess));
 
-const hbs = exphbs.create({});
+ const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
